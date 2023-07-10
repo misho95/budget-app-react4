5e6adfa74bd9198c1a6bd3b25953dfb88160e393
Misho <chapidze.misho95@gmail.com>
2023-07-10 16:56:26 +0400

    ტესტ

---
+++ src/components/statisticstopincome.jsx
@@ -0,0 +1,27 @@
+function StatisticsTopIncome(){
+    return(
+        <div className="bg-white text-gray-600 shadow-sm p-3 rounded-lg w-full sm:w-fit">
+        <div className="flex flex-col">
+            <div>
+                <p className="text-sm text-gray-400">3 ყველაზე დიდი შემოსავალი</p>
+            </div>
+           <div className="flex flex-col">
+                <div className="flex flex-col gap-1 ">
+                    <p> გადარიცხვა: 5069</p>
+                    <div className="w-4/5 h-2 bg-green-500"></div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <p> ინვოისი: 592</p>
+                    <div className="w-1/5 h-2 bg-orange-500"></div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <p> სხვა: 2051</p>
+                    <div className="w-1/2 h-2 bg-orange-500"></div>
+                </div>
+           </div>
+        </div>
+    </div>
+    )
+}
+
+export default StatisticsTopIncome;
